c691d151eee3f99c3a851e77fc916b15b25dd0f5
cmstill <159965182+cmstill@users.noreply.github.com>
2024-04-18 18:53:53 -0500

    April 18, 6:53pm
    Added constants file for TODOS_COLLECTION
    
    Changes Database class to include dbToDos function that returns whatever db we're connected to and whatever collection is saved to TODOS_COLLECTION in constants
    
    Added support for GET one todo at a time from DB, PATCH, PUT, and DELETE

--- controllers/todos.controllers.js
+++ controllers/todos.controllers.js
@@ -9,58 +9,75 @@ import ToDosCoordinator from "../coordinator/todos.coordinator.js";
  */
 
 export const getToDos = async (req, res, next) => {
-	const result = await ToDosCoordinator.getToDos(); // why is this awaited?
 
-	res.status(200).json(result);
+	try {
+		const result = await ToDosCoordinator.getToDos(); // why is this awaited?
+
+		res.status(200).json(result);
+	} catch (ex) {
+		next(ex);
+	};
 };
 
-export const getToDo = async (req, res, next ) => {
-	const result = ToDosCoordinator.getToDo(req.params.id);
+export const getToDo = async (req, res, next) => {
+	try {
+		const result = await ToDosCoordinator.getToDo(req.params.id); // this needs to be awaited now because our getToDo function in model is now returning a promise
 
-	if (result) { 
-		res.status(200).json(result);
-	} else {
-		res.status(404).json('The ToDo you requested does not exist');
+		if (result) {
+			res.status(200).json(result);
+		} else {
+			res.status(404).json('The ToDo you requested does not exist');
+		};
+	} catch (ex) {
+		next(ex);
 	};
 };
 
 export const createToDo = async (req, res, next) => {
-	
+
 	try { // this is all to catch the errors that will be picked up by our error handler middleware
-		const result = ToDosCoordinator.createToDo(req.body); 
+		const result = ToDosCoordinator.createToDo(req.body);
 		res.status(201).json(result);
-	} catch(ex) { // ex here is a variable that holds the error that gets generated when an error occurs
+	} catch (ex) { // ex here is a variable that holds the error that gets generated when an error occurs
 		next(ex); // this this is where you pass that error in variable ex so it can be accessed
-	}
+	};
 };
 
 export const replaceToDo = async (req, res, next) => {
-	const result = ToDosCoordinator.replaceToDo(req.params.id, req.body);
+	try {
+		const result = await ToDosCoordinator.replaceToDo(req.params.id, req.body);
 
-	if (result) {
-		res.status(200).json(result);
-	} else {
-		res.status(404).json('The ToDo you requested does not exist')
+		if (result) {
+			res.status(200).json(result);
+		} else {
+			res.status(404).json('The ToDo you requested does not exist')
+		};
+	} catch (ex) {
+		next(ex);
 	};
 };
- 
+
 export const updateToDo = async (req, res, next) => {
-	const result = ToDosCoordinator.updateToDo(req.params.id, req.body)
 
-	if (result) {
-		res.status(200).json(result);
-	} else {
-		res.status(404).json('The ToDo you requested does not exist')
-	};
+	try {
+		const result =  ToDosCoordinator.updateToDo(req.params.id, req.body)
+
+		if (result) {
+			res.status(200).json(result);
+		} else {
+			res.status(404).json('The ToDo you requested does not exist')
+		};
+	} catch (ex) {
+		next(ex);
+	}
 };
 
-export const deleteToDo = async (req, res, next) => { 
-	const result = ToDosCoordinator.deleteToDo(req.params.id);
+export const deleteToDo = async (req, res, next) => {
 
-	if (result) {
-		res.status(204).json(result);
-	} else {
-		res.status(404).json('The ToDo you requested does not exist')
-	};
+	try {
+		await ToDosCoordinator.deleteToDo(req.params.id);
+		res.status(204).json();
+	} catch (ex) {
+		next(ex);
+	}
 };
-	
--- models/todos.model.js
+++ models/todos.model.js
@@ -1,4 +1,5 @@
-import db  from '../lib/database.js';
+import db from '../lib/database.js';
+import Constants from '../lib/constants.js';
 
 let toDos = [
 	{
@@ -26,73 +27,76 @@ export default class ToDosModel {
 	static getToDos = async () => {
 		console.log('Got full list of ToDos:');
 		//return toDos;
-		
-		return db.getDb().collection('todos').find({}).toArray();// this is returning a promise and that goes all the way up to the controller that is awaiting that promise
+
+		return db.getDb().collection(Constants.TODOS_COLLECTION).find({},
+			{ projection: Constants.DEFAULT_PROJECTION }).toArray();// this is returning a promise and that goes all the way up to the controller that is awaiting that promise
+		//and db.getDb() is calling the getDb() function on the Database class (saved to db)
 	};
 
-/**
- * createToDo - accepts newToDo, an object that starts out as req.body, adds that ToDo to DB, returns newToDo 
- * @param {Object} newToDo - new ToDo to create in DB
- * @returns {Object} - An object that holds properties of new ToDo
- */
-	static createToDo = (newToDo) => {
-		toDos.push(newToDo);
+	/**
+	 * createToDo - accepts newToDo, an object that starts out as req.body, adds that ToDo to DB, returns newToDo 
+	 * @param {Object} newToDo - new ToDo to create in DB
+	 * @returns {Object} - An object that holds properties of new ToDo
+	 */
+	static createToDo = async (newToDo) => {
 		console.log('Created new ToDo:');
 		console.log(newToDo);
-		return newToDo;
+		await db.getDb().collection(Constants.TODOS_COLLECTION).insertOne(newToDo);
+		const returnToDo = { ...newToDo };
+		delete returnToDo._id;// this line and one above creates a copy of newToDo, saves that to returnToDo, then deletes the _id property off that so the user doesn't see it. This is because we don't want to mutate paramaters that are passed to our functions in those functions. 
+		return returnToDo;
 	};
 
 	static getToDo = (id) => {
-		const todo = toDos.find((item) => {
-			return item.id === id;
-		});
+		// const todo = toDos.find((item) => {
+		// 	return item.id === id;
+		// });
 
-		console.log(`Got toDo by ID provided:`);
-		console.log(todo);
+		console.log(`Got toDo by ID provided`);
 
-		return todo;
+		// return todo;
+		return db.getDb().collection(Constants.TODOS_COLLECTION).findOne({ "id": id }, { projection: Constants.DEFAULT_PROJECTION });
 	};
 
-	static replaceToDo = (id, replacedToDo) => {
-		const toDoIndex = toDos.findIndex((item) => (item.id === id));
+	static replaceToDo = async (id, replacedToDo) => { // this isn't implimented oftentimes.  You usually only want to update certain properties
+		const result = await db.getDb().collection(Constants.TODOS_COLLECTION).replaceOne({ id }, replacedToDo);
 
-		if (toDoIndex > -1) {
-			console.log(`Replaced toDo at ${id}`, 'with:', replacedToDo);
-			toDos.splice(toDoIndex, 1, replacedToDo);
+		if (result.matchedCount === 1) {
+			return replacedToDo;
+		}
+
+		return false; // this is so in the controller, I know I got something flasey (null) and so I faild and can throw an error
+	};
+
+	static updateToDo = async (id, toDo) => {
+
+		const update = {
+			$set: {
+				//all this allows us to dynamically accept a property from req.body on the patch route that populates here in the model
+			},
+		};
+
+		Object.keys(toDo).forEach((key) => {
+			if (key === 'id') {
+				return;
+			}
+
+			update.$set[key] = toDo[key]; // what is going on here?
+		});
+
+		const result = db.getDb().collection(Constants.TODOS_COLLECTION).updateOne({ id }, update);
+
+		if (result.matchedCount === 1) {
 			return replacedToDo;
 		}
 
 		return false;
 	};
 
-	static updateToDo = (id, toDo) => {
-		const toDoIndex = toDos.findIndex((item) => (item.id === id));
+	static deleteToDo = (id) => { // on postman, this hangs up but on the backend prints what I deleted to the console.  Also the deletion does happen. 
 
-		if (toDoIndex > -1) {
-			Object.keys(toDo).forEach((key) => {
-				if (key === 'id') {
-					return;
-				}
-				toDos[toDoIndex][key] = toDo[key];
-			});
-			console.log(`Updated ToDo at ${id}`, toDos[toDoIndex]);
-			return toDos[toDoIndex];
-		}
+		console.log(`Deleted todo with ID of: ${id}`);
 
-		return false;
-	};
-
-	static deleteToDo = (id) => {
-		const countBeforeDelete = toDos.length;
-
-		toDos = toDos.filter((item) => (item.id !== id));
-
-		if (countBeforeDelete === toDos.length) {
-			return false;
-		}
-
-		return true;
+		return db.getDb().collection(Constants.TODOS_COLLECTION).deleteOne({ id });
 	};
 }
-
-
